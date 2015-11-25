0e88cf41c284fc1396095d8464a64cfe03219fd1
Ben Carpenter <carpenter@earthlinginteractive.com>
2015-11-25 10:20:14 -0600

    vagrant is all set for node and mongo

---
+++ puppet/modules/mongodb/manifests/init.pp
@@ -0,0 +1,29 @@
+# == Class: nodejs
+#
+# Class defined to create sites and their configuration based on a give template
+#
+class mongodb {
+
+  exec { 'add-public-key':
+    command => 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10'
+  }
+
+  exec { 'add-apt-repository-mongo':
+    creates => '/etc/apt/sources.list.d/mongodb-org-3.0.list',
+    command => 'echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list',
+    require => Exec['add-public-key']
+  }
+
+  exec { 'apt-get-mongo':
+    command => '/usr/bin/apt-get update',
+    require => Exec['add-apt-repository-mongo']
+  }
+
+  package { 'mongodb-org':
+    ensure  => present,
+    require => [
+      Exec['apt-get-mongo'],
+      Exec['add-apt-repository-mongo']
+    ]
+  }
+}
